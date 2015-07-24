d08a7fe59cdf0c102b07248460210c482eb8d916
vafus <alejandro183@gmail.com>
2015-07-23 19:17:36 -0500

    new function to intersec polygons and linestring returning the linestring by segments

--- index.js
+++ index.js
@@ -380,6 +380,148 @@ var getLineSegments = function(queryParams, callback){
 }
 
 /**
+ * This method is applicable only to linestrings. 
+ * Creates a geojson with segments of line. 
+ * Additionally the distance (distance) and velocity (segmentVelocity) of each segment are estimated.  
+ * @param {Object} geometryColumn
+ * @param {Object} tableName
+ */ 
+var getIntersectingSegments = function(queryParams, callback){
+	console.log("executing getIntersectingSegments ");
+	var geojson = {
+			"type" : "FeatureCollection",
+			"features" : []
+		};
+	var columns = [];
+	if(queryParams.properties.constructor === Array )
+	{
+		columns = queryParams.properties;
+	}
+	//Mysql query
+	if (credentials.type === 'mysql') {
+		console.log("getLineSegments NOT IMPLEMENTED for mysql db");
+		
+		/*var query = 'SELECT *, AsWKT(' + queryParams.geometry + ') AS wkt FROM ' + queryParams.tableName;
+		var queryCon = connection.query(query);
+		queryCon
+			.on('result', function(row){
+				var geometry = wkt.parse(row.wkt);
+				var properties = {};
+				for(i in columns)
+				{
+					var col = columns[i];
+					properties[col] = row[col];
+				}
+				var feature = {
+					"type" : "Feature",
+					"geometry" : geometry,
+					"properties" : properties
+				};
+				geojson.features.push(feature);
+			})
+			.on('fields', function(fields){
+				if(queryParams.properties == 'all')
+				{
+					for (i in fields) {
+						var name = fields[i].name;
+						if (name != queryParams.geometry && name != 'wkt')
+							columns.push(fields[i].name);
+					}
+				}
+				//console.log(columns);
+			})
+			.on('end', function(){
+				//console.log('se acabo...');
+				//console.log(geojson);
+				callback(geojson);
+			});*/
+			
+	} else if(credentials.type === 'postgis'){
+
+		var connectString = 'postgres://' + credentials.user + ':' + credentials.password + '@' + credentials.host + '/' + credentials.database;
+		console.log("Query to PostGis");
+		//console.log(connectString);
+		connection = new pg.Client(connectString);
+		connection.connect(function(err) {
+			if (err) {
+				return console.error('could not connect to postgres', err);
+			}
+			console.log('connected');
+			
+			//var query = 'SELECT *, ST_AsText(' + queryParams.geometry + ') AS wkt FROM ' + queryParams.tableName + ' LIMIT ' + queryParams.limit;
+			//var query = 'SELECT *, ST_AsText(' + queryParams.geometry + ') AS wkt FROM ' + queryParams.tableName;
+			var query = 'SELECT *, ST_AsText(' + queryParams.geometry + ') AS wkt FROM ' + queryParams.tableName;
+			//var query = "SELECT DISTINCT routes.id, ST_AsText('routes.trajectory') AS wkt FROM redprimariawgs84_lite_buffer, routes WHERE (routes.time_stamp BETWEEN '2004-01-06 00:00:00' AND '2004-01-06 00:59:59') AND (redprimariawgs84_lite_buffer.ogc_fid=1)	AND (ST_Intersects(routes.trajectory, redprimariawgs84_lite_buffer.wkb_geometry))";
+			
+			//"SELECT DISTINCT routes.id, routes.trajectory FROM redprimariawgs84_lite_buffer, routes WHERE (routes.time_stamp BETWEEN '2004-01-06 00:00:00' AND '2004-01-06 00:59:59') AND (redprimariawgs84_lite_buffer.ogc_fid=1)	AND (ST_Intersects(routes.trajectory, redprimariawgs84_lite_buffer.wkb_geometry))"
+						
+			
+			/*if(queryParams.dateColumn != undefined && queryParams.dateRange != undefined){ 
+				query += ' WHERE ' + queryParams.dateColumn + ' BETWEEN ' + queryParams.dateRange;			
+			}*/
+			
+			if(queryParams.limit != undefined){				
+				query += ' LIMIT ' + queryParams.limit;
+			}
+			
+			console.log("PostGIS query");
+			//console.log(query);
+			connection.query(query, function(err, result) {
+				if (err) {
+					console.log('error')
+					console.log(err.stack);
+				}
+
+		
+				/*if(queryParams.properties == 'all')
+				{
+					for(field in result.fields){
+						var name = result.fields[field].name;
+						if (name != queryParams.geometry && name != 'wkt')
+							columns.push(result.fields[field].name);
+					}
+				}*/
+				console.log("PostGIS query executed! " + result.rows.length);
+				console.log("PostGIS query executed! " + result.rows);
+				var totalDistance = 0;
+				for (each in result.rows) {
+										
+					var Terraformer = require('terraformer');
+					var WKT = require('terraformer-wkt-parser');								
+					var geometry = WKT.parse(result.rows[each].wkt);					
+					var numOfSegments = geometry.coordinates.length -1 ;
+					
+					for(var coordinate=0; coordinate < numOfSegments; coordinate++){											
+						var properties = {};
+						/*for(i in columns){
+							var col = columns[i];
+							properties[col] = result.rows[each][col];
+						}*/						
+						var segmentDistance = getDistance(geometry.coordinates[coordinate],geometry.coordinates[coordinate+1]);
+						var segment = new Terraformer.LineString([ geometry.coordinates[coordinate],geometry.coordinates[coordinate+1] ]);
+						properties["segmentLength"] = segmentDistance;
+						/*properties["segmentVelocity"] = (segmentDistance/1000) / (dt/3600);						 
+						totalDistance+=segmentDistance;*/
+						var feature = {
+							"type" : "Feature",
+							"geometry" : segment,
+							"properties" : properties
+						};
+						geojson.features.push(feature);		
+					}
+				}				
+				callback(geojson);
+			});			
+			//connection.end();			
+		});
+		
+	} else {
+		throw "there is no valid db type. [type] = " + credentials.type;
+	}
+
+}
+
+/**
  * Calculates the distance of two points in meters. 
  * The point must be in lattitude and longitude.
  * @param {Object} latlng1 : point 1
@@ -408,6 +550,7 @@ module.exports = {
 	geoQuery : geoQuery,
 	//geoQueryLimited : geoQueryLimited,
 	getLineSegments : getLineSegments,
+	getIntersectingSegments : getIntersectingSegments,
 	getDistance : getDistance
 	//testFunction : testFunction	
 }
