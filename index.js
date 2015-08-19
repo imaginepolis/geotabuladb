c50b2a6f2f5ea6157b60fec747daef30ecee1fc1
jatriana <alejandro183@gmail.com>
2015-08-19 16:22:20 -0500

    GeotabulaDB expose end connection functionality

--- index.js
+++ index.js
@@ -160,7 +160,7 @@ var query = function(queryParams, callback) {
 						
 			var query = 'SELECT ';
 			for(col in columns){				
-				query += columns[col] ;
+				query += columns[col] ;where
 				if(col<columns.length-1){
 					query += ', ';
 				}
@@ -348,7 +348,7 @@ module.exports = {
 	setCredentials : setCredentials,
 	logCredentials : logCredentials,
 	connectToDb : connectToDb,
-	//endConnection : endConnection,
+	endConnection : endConnection,
 	geoQuery : geoQuery,
 	query : query,
 	testFunction : testFunction
