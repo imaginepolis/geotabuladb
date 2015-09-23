b793c9b4492a3d67286552d64a738d59c92fc527
Camilo Vargas Cabrera <vargax@gmail.com>
2015-09-23 15:19:03 +0200

    GeoQuery tested and working...

--- index.js
+++ index.js
@@ -110,10 +110,11 @@ var GeotabulaDB = (function () {
                     var columns = [];
                     for (var column in result.fields) {
                         var _name = result.fields[column].name;
-                        if (_name != 'wkt') {
+                        if (_name != 'wkt' && _name != queryParams.geometry) {
                             columns.push(_name);
                         }
                     }
+                    console.dir(columns);
 
                     var geojson = {
                         "type": "FeatureCollection",
@@ -122,9 +123,31 @@ var GeotabulaDB = (function () {
 
                     for (var row in result.rows) {
                         var properties = {};
-                        for (var column in columns) {
-                            properties[column] = result.rows[row][column];
+                        var _iteratorNormalCompletion2 = true;
+                        var _didIteratorError2 = false;
+                        var _iteratorError2 = undefined;
+
+                        try {
+                            for (var _iterator2 = columns[Symbol.iterator](), _step2; !(_iteratorNormalCompletion2 = (_step2 = _iterator2.next()).done); _iteratorNormalCompletion2 = true) {
+                                var column = _step2.value;
+
+                                properties[column] = result.rows[row][column];
+                            }
+                        } catch (err) {
+                            _didIteratorError2 = true;
+                            _iteratorError2 = err;
+                        } finally {
+                            try {
+                                if (!_iteratorNormalCompletion2 && _iterator2['return']) {
+                                    _iterator2['return']();
+                                }
+                            } finally {
+                                if (_didIteratorError2) {
+                                    throw _iteratorError2;
+                                }
+                            }
                         }
+
                         var geometry = wkt.parse(result.rows[row]['wkt']);
                         var feature = {
                             'type': 'Feature',
@@ -213,7 +236,7 @@ var ParserHelper = (function () {
             var log = '.genGeoQueryString()';
 
             var query = ParserHelper.genSelectString(queryParams.properties);
-            query += ', ST_AsText(' + queryParams.geomertry + ') AS wkt';
+            query += ', ST_AsText(' + queryParams.geometry + ') AS wkt';
             query += ParserHelper.genFromString(queryParams);
 
             console.log(logString + log + logOK + query);
